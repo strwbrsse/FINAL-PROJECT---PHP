1b0dd5da35ebf84e4b5e97a7b721124e9d8cf6d8
Caresse <caressem12@gmail.com>
2024-10-18 10:07:40 +0800

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv= "X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width-device-width, initial-scale=1.0">
+        <title>Registartion Form</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="style_r.css">
+    </head>
+    <body>
+        <div class="container">
+            <forms action="registration.php" method="post">
+                <div class="form-group">
+                    <input type ="text" class="form.control" name="fullname" placeholder="Full Name:">
+                </div>
+                <div class="form-group">
+                    <input type ="email" class="form.control" name="email" placeholder="Email:">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form.control" name="password" placeholder="Password:">
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form.control" name="repeat_password" placeholder="Repeat Password:">
+                </div>
+                <div class ="form-btn">
+                    <input type ="submit" class="btn btn-primary" value="Register" name="submit">
+                </div>
+            </forms>
+        </div>
+    </body>
+</html>
