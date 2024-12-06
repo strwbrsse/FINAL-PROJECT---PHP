4733b95167cfba699c2c7da80a38bcce0467d92e
gAccnt <gaccnt052423@gmail.com>
2024-12-07 04:04:41 +0800

    Add disease and allergy description

--- UserDashboard/getProfile.php
+++ UserDashboard/getProfile.php
@@ -9,8 +9,9 @@ if ($conn->connect_error) {
     if (isset($_POST['userId'])) {
         $userId = $_POST['userId'];
         $sql = "SELECT un.fname, un.mname, un.lname, p.birthday, c.email, c.contact, a.address, a.barangay, p.sex, 
-        p.civilstat, e.employment_stat, e.employer, e.profession FROM user_name as un join personal as p on un.name_id = p.name_id 
+        p.civilstat, e.employment_stat, e.employer, e.profession, h.allergy_description, h.disease_description  FROM user_name as un join personal as p on un.name_id = p.name_id 
         join contact as c on p.name_id = c.name_id join address as a on c.name_id = a.name_id join employment as e on a.name_id = e.name_id 
+         join health as h on e.name_id = h.name_id
         WHERE un.name_id = $userId";
         $result = mysqli_query($conn, $sql);
         if (mysqli_num_rows($result) === 1) {
--- UserDashboard/updateProfile.php
+++ UserDashboard/updateProfile.php
@@ -7,7 +7,7 @@
     if ($_POST['userId'] && isset($_POST['fname']) && isset($_POST['mname']) && isset($_POST['lname']) && isset($_POST['birthday'])
         && isset($_POST['mail']) && isset($_POST['contact']) && isset($_POST['address']) && isset($_POST['barangay'])
         && isset($_POST['sex']) && isset($_POST['civilstat']) && isset($_POST['employmentstat']) && isset($_POST['employer'])
-        && isset($_POST['profession']) && isset($_POST['userId'])) {
+        && isset($_POST['profession']) && isset($_POST['userId']) && isset($_POST['allergy']) && isset($_POST['disease'])) {
 
         $userId = $_POST['userId'];
         $fname = $_POST['fname'];
@@ -23,14 +23,18 @@
         $employmentstat = $_POST['employmentstat'];
         $employer = $_POST['employer'];
         $profession = $_POST['profession'];
+        $allergy = $_POST['allergy'];
+        $disease = $_POST['disease'];
 
         $sql = "UPDATE user_name as un join personal as p on un.name_id = p.name_id 
             join contact as c on p.name_id = c.name_id 
             join address as a on c.name_id = a.name_id 
             join employment as e on a.name_id = e.name_id 
+            join health as h on e.name_id = h.name_id
             set un.fname = '$fname', un.mname = '$mname', un.lname = '$lname', p.birthday = '$birthday', c.email = '$mail',
             c.contact = '$contact', a.address = '$address', a.barangay = '$barangay', p.sex = '$sex', 
-            p.civilstat = '$civilstat', e.employment_stat = '$employmentstat', e.employer = '$employer', e.profession = '$profession'
+            p.civilstat = '$civilstat', e.employment_stat = '$employmentstat', e.employer = '$employer', e.profession = '$profession',
+            h.allergy_description = '$allergy', h.disease_description = '$disease'
             WHERE p.personal_id = $userId";
 
         if(mysqli_query($conn, $sql)){
